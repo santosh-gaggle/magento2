cccfb0370992a8a683d5bb58a10f41d7e989ae49
Lukasz Bajsarowicz <lukasz.bajsarowicz@gmail.com>
2020-04-25 01:36:26 +0000

    #27500 Migrate PHPUnit tests for Shell module

--- lib/internal/Magento/Framework/Shell/Test/Unit/ComplexParameterTest.php
+++ lib/internal/Magento/Framework/Shell/Test/Unit/ComplexParameterTest.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
@@ -6,9 +6,10 @@
 
 namespace Magento\Framework\Shell\Test\Unit;
 
-use \Magento\Framework\Shell\ComplexParameter;
+use Magento\Framework\Shell\ComplexParameter;
+use PHPUnit\Framework\TestCase;
 
-class ComplexParameterTest extends \PHPUnit\Framework\TestCase
+class ComplexParameterTest extends TestCase
 {
     public function testGetFromArray()
     {
