03444786c242662cb23b11ee6782ed8c3fabbea0
Lukasz Bajsarowicz <lukasz.bajsarowicz@gmail.com>
2020-04-25 01:57:28 +0000

    #27500 Migrate PHPUnit tests for Encryption module

--- lib/internal/Magento/Framework/Encryption/Test/Unit/Helper/SecurityTest.php
+++ lib/internal/Magento/Framework/Encryption/Test/Unit/Helper/SecurityTest.php
@@ -1,10 +1,11 @@
-<?php declare(strict_types=1);
+<?php
 /**
  * Collection of various useful functions
  *
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
 
 /**
  * Test case for \Magento\Framework\Encryption\Security
